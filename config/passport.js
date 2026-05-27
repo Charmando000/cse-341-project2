92c99cfbc368c1159562f6820003620ff18c8685
Charmando000 <elescogido0310@gmail.com>
2026-05-27 19:09:42 -0300

    Fixed OAuth callback configuration

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
+      callbackURL: process.env.NODE_ENV === 'production'
+  ? 'https://cse-341-project2-ilsl.onrender.com/auth/google/callback'
+  : 'http://localhost:3000/auth/google/callback'
     },
 
     (accessToken, refreshToken, profile, done) => {
