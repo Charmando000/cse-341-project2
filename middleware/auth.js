3dc2d6214e1f6ece1b15595f5fcb4b64061cddec
Charmando000 <elescogido0310@gmail.com>
2026-05-28 14:49:44 -0300

    Fixed auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,11 @@
 const isAuthenticated = (req, res, next) => {
-
-  if (req.session.passport.user) {
-    next();
-  } else {
-    res.status(401).json({
-      message: 'You must log in'
-    });
+  if (req.isAuthenticated()) {
+    return next();
   }
+
+  res.status(401).json({
+    message: 'You must log in'
+  });
 };
 
 module.exports = isAuthenticated;
